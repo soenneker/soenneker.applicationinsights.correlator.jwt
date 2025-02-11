d0b0765b1cc2890e4c5cf358aa124b9387db70fd
Jake Soenneker <jake@soenneker.com>
2025-02-11 13:03:39 -0600

    Fluent

--- src/Registrars/JwtTelemetryCorrelatorRegistrar.cs
+++ src/Registrars/JwtTelemetryCorrelatorRegistrar.cs
@@ -1,4 +1,4 @@
-using Microsoft.ApplicationInsights.Extensibility;
+﻿using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.ApplicationInsights.Correlator.Jwt.Abstract;
@@ -13,9 +13,10 @@ public static class JwtTelemetryCorrelatorRegistrar
     /// <summary>
     /// Adds <see cref="IJwtTelemetryCorrelator"/> as a singleton service. <para/>
     /// </summary>
-    public static void AddJwtTelemetryCorrelatorAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddJwtTelemetryCorrelatorAsSingleton(this IServiceCollection services)
     {
         services.AddHttpContextAccessor();
         services.TryAddSingleton<ITelemetryInitializer, JwtTelemetryCorrelator>();
+        return services;
     }
 }
