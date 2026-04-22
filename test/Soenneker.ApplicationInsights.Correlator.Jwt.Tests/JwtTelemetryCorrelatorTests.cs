ff1f53997d1824cc5b2ea34a1f0caeda71ebefec
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:10 -0500

    TUnit conversion

--- test/Soenneker.ApplicationInsights.Correlator.Jwt.Tests/JwtTelemetryCorrelatorTests.cs
+++ test/Soenneker.ApplicationInsights.Correlator.Jwt.Tests/JwtTelemetryCorrelatorTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.ApplicationInsights.Correlator.Jwt.Tests;
 
-[Collection("Collection")]
-public class JwtTelemetryCorrelatorTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class JwtTelemetryCorrelatorTests : HostedUnitTest
 {
-    public JwtTelemetryCorrelatorTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public JwtTelemetryCorrelatorTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
